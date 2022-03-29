114b5888c2733a512aaa3add4ad089361423948f
Suneesh-S <122005142@sastra.ac.in>
2022-03-29 22:25:09 +0530

    Project code uploaded

---
+++ 3_Implementation/inc/print_function.h
@@ -0,0 +1,12 @@
+#ifndef __PRINT_FUNCTION_H
+#define __PRINT_FUNCTION_H
+void resistor_code_print();
+void ohms_law_print();
+void LED_series_print();
+void battery_life_print();
+
+/////////////////////////////////////////////
+
+
+
+#endif
